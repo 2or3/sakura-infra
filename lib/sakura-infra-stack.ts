91e56f5c4f5312dfc6f5f974561f1decd4a596ff
2or3 <t.nagashima.cog@gmail.com>
2021-11-20 00:48:48 +0900

    add S3 resorce

--- lib/sakura-infra-stack.ts
+++ lib/sakura-infra-stack.ts
@@ -1,11 +1,14 @@
 import * as cdk from '@aws-cdk/core';
-// import * as sqs from '@aws-cdk/aws-sqs';
+import * as s3 from '@aws-cdk/aws-s3';
 
 export class SakuraInfraStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
     // The code that defines your stack goes here
+    const bucket = new s3.Bucket(this, "SakuraOriginBucket", {
+      bucketName: this.node.tryGetContext('originBucketName')
+    })
 
     // example resource
     // const queue = new sqs.Queue(this, 'SakuraInfraQueue', {
